4f1a5b2f38dcf09e8a0fc690bb2fd72f18497ce6
Guillaume Migeon <guillaumeoutpox@gmail.com>
2022-09-27 14:48:05 +0200

    set default variable for regexp functions that returns an array

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Device } from './components/device';
 
 export function parseBluetoothStatus(output: string) {
-  const powered = /Powered: (.*)/.exec(output)![1] === 'yes' || false;
+  const powered = (/Powered: (.*)/.exec(output) ?? [])[1] === 'yes' || false;
   return powered
     ? 'ON'
     : 'OFF';
@@ -11,16 +11,17 @@ export function parseDevices(output: string): PairedDevices[] {
   return [...output.matchAll(/Device (([0-9A-F]{2}[:-]){5}([0-9A-F]{2})) (.*)$/gmi)]
     .map(captureGroups => ({
       mac: captureGroups[1],
-      name: captureGroups[4],
+      name: captureGroups[4] || 'Unnamed device',
     }));
 }
 
 export function parseDevicesInfo(output: string[]): Device[] {
+  // Might be interesting to return the alias instead of name if defined.
   return output.map(device => ({
     mac: /Device (([0-9A-F]{2}[:-]){5}([0-9A-F]{2}))/.exec(device)![1],
-    name: /Name: (.*)/.exec(device)![1],
+    name: (/Name: (.*)/.exec(device) ?? [])[1] || 'Unnamed device',
     connected: /Connected: yes/.test(device),
-    icon: /Icon: (.*)/.exec(device)![1],
+    icon: (/Icon: (.*)/.exec(device) ?? [])[1] || '',
   }));
 }
 
