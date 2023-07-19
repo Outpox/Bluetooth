f66c4b052f18ec7a83142c5bf9e8f8bf449b6d12
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-07-19 10:37:20 +0200

    Implement new command version for bluez 5.66
    The syntax for returning the paired devices has changed in Bluez 5.66, and on main the list is empty without this change.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
@@ -8,7 +9,11 @@ class Plugin:
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
     async def get_paired_devices(self):
-        devices = subprocess.run(["bluetoothctl", "paired-devices"],timeout=10, text=True, capture_output=True).stdout
+        bctl_version = re.split(r'[0-9]+', subprocess.run(["bluetoothctl", "version"], timeout=10, text=True, capture_output=True).stdout)
+        if len(bctl_version) == 2 and int(bctl_version[0]) >= 5 and int(bctl_version[1]) >= 66:
+            devices = subprocess.run(["bluetoothctl", "devices", "Paired"], timeout=10, text=True, capture_output=True).stdout
+        else
+            devices = subprocess.run(["bluetoothctl", "paired-devices"],timeout=10, text=True, capture_output=True).stdout
         return devices
 
     async def get_device_info(self, device):
