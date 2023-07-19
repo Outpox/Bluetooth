07eb5d2698c4eabfecc63b5fd93247f9ef06c8fb
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-07-19 11:25:12 +0200

    Export keyboard icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,3 +13,7 @@ export const HeadsetIcon = Object.values(IconsModule).find((mod: any) =>
 export const GamepadIcon = Object.values(IconsModule).find((mod: any) =>
   mod?.toString && /d:\"M32.62 9.14C32.62 9.14 28.5 5 18 5C7.5 /.test(mod.toString())
 ) as FC<SVGAttributes<SVGElement>>;
+
+export const KeyboardIcon = Object.values(IconsModule).find((mod: any) =>
+  mod?.toString && /\.createElement\(\"path\",{d:\"M2 9H34V27H2V9ZM5 /.test(mod.toString())
+) as FC<SVGAttributes<SVGElement>>;
