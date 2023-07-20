39e9b6a0a77a87a6460f052746916c9fe95a4e0d
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-07-20 14:06:43 +0200

    Correctly validate bctl version in case of a mayor release

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
     async def get_paired_devices(self):
         bctl_version = re.findall(r'[0-9]+', subprocess.run(["bluetoothctl", "version"], timeout=10, text=True, capture_output=True).stdout)
-        if len(bctl_version) == 2 and int(bctl_version[0]) >= 5 and int(bctl_version[1]) >= 66:
+        # Check for bluetoothctl version greater or equal to 5.66 (always true if mayor is > 5)
+        if len(bctl_version) == 2 and (int(bctl_version[0]) > 5 or (bctl_version[0] == 5 and int(bctl_version[1]) >= 66)):
             devices = subprocess.run(["bluetoothctl", "devices", "Paired"], timeout=10, text=True, capture_output=True).stdout
         else:
             devices = subprocess.run(["bluetoothctl", "paired-devices"],timeout=10, text=True, capture_output=True).stdout
