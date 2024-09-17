fc7bf734139e877e64f89067465260c0fc262c68
Guillaume Migeon <guillaumeoutpox@gmail.com>
2024-09-17 19:59:28 +0200

    move scss module declaration in existing file

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,19 @@
-declare module "*.svg" {
+declare module '*.scss' {
   const content: string;
   export default content;
 }
 
-declare module "*.png" {
+declare module '*.svg' {
   const content: string;
   export default content;
 }
 
-declare module "*.jpg" {
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
   const content: string;
   export default content;
 }
