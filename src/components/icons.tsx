219b54fc001536352539efcf8e8e1daeeb1ebcb5
Potaperper <zhangkaiming.2001@foxmail.com>
2026-05-05 00:33:37 +0800

    fix: replace broken IconsModule regex matching with inline SVG icons
    The plugin used Object.values(IconsModule).find() with regex matching on
    .toString() output to locate icon components from decky-frontend-lib's
    IconsModule. This approach is fragile — when the internal representation
    of icons changes in newer versions of decky-frontend-lib or the Steam
    client, the regex patterns no longer match, causing all icon lookups to
    return undefined.
    
    When React attempts to render an undefined component (e.g. <BluetoothIcon/>),
    it throws an uncaught exception that crashes the entire Decky Loader UI,
    not just the plugin. Users see "An error occurred while rendering this
    content" and lose access to all Decky functionality.
    
    Fixes #15
    Fixes decky-loader#903
    
    Changes:
    - src/components/icons.tsx: Replace IconsModule regex matching with
      hardcoded SVG icon components (Bluetooth, Headset, Gamepad, Keyboard)
    - plugin.json: Remove debug flag (unrelated cleanup)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,19 +1,31 @@
-/* eslint-disable no-useless-escape */
 import { FC, SVGAttributes } from 'react';
-import { IconsModule } from 'decky-frontend-lib';
 
-export const BluetoothIcon = Object.values(IconsModule).find((mod: any) =>
-  mod?.toString && /\.createElement\(\"path\",{d:\"M29.41 /.test(mod.toString())
-) as FC<SVGAttributes<SVGElement>>;
+// Hardcoded SVG icons to replace broken IconsModule regex matching.
+// The previous implementation used Object.values(IconsModule).find() with
+// regex matching on .toString() output, which fails when the internal
+// representation of icons changes in newer versions of decky-frontend-lib
+// or the Steam client, causing the entire Decky Loader UI to crash.
 
-export const HeadsetIcon = Object.values(IconsModule).find((mod: any) =>
-  mod?.toString && /\.createElement\(\"path\",{d:\"M32 18.05C32 /.test(mod.toString())
-) as FC<SVGAttributes<SVGElement>>;
+export const BluetoothIcon: FC<SVGAttributes<SVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M14.24 12.01l2.32 2.32c.28-.72.44-1.51.44-2.33 0-.82-.16-1.59-.43-2.31l-2.33 2.32zm5.29-5.3l-1.26 1.26c.63 1.21.98 2.57.98 4.02s-.36 2.82-.98 4.02l1.2 1.2A9.936 9.936 0 0021 12.01c0-1.96-.56-3.78-1.54-5.32l.07.02zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
+  </svg>
+);
 
-export const GamepadIcon = Object.values(IconsModule).find((mod: any) =>
-  mod?.toString && /d:\"M32.62 9.14C32.62 9.14 28.5 5 18 5C7.5 /.test(mod.toString())
-) as FC<SVGAttributes<SVGElement>>;
+export const HeadsetIcon: FC<SVGAttributes<SVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M12 1c-4.97 0-9 4.03-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7c0-4.97-4.03-9-9-9z"/>
+  </svg>
+);
 
-export const KeyboardIcon = Object.values(IconsModule).find((mod: any) =>
-  mod?.toString && /d:\"M2 9H34V27H2V9ZM5 /.test(mod.toString())
-) as FC<SVGAttributes<SVGElement>>;
+export const GamepadIcon: FC<SVGAttributes<SVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M21.58 16.09l-1.09-7.66C20.21 6.46 18.52 5 16.53 5H7.47C5.48 5 3.79 6.46 3.51 8.43l-1.09 7.66C2.2 17.63 3.39 19 4.94 19h0c.68 0 1.32-.27 1.8-.75L9 16h6l2.25 2.25c.48.48 1.13.75 1.8.75h0c1.56 0 2.75-1.37 2.53-2.91zM11 11H9v2H8v-2H6v-1h2V8h1v2h2v1zm4 2c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm2-3c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"/>
+  </svg>
+);
+
+export const KeyboardIcon: FC<SVGAttributes<SVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"/>
+  </svg>
+);
