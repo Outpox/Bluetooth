fdf8708ded21a0d1c3a4254838efdf0e72341df0
Outpox <outpox@outpox.fr>
2026-08-13 19:03:49 +0200

    style: format source with prettier

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,25 +9,25 @@ import { FC, SVGAttributes } from 'react';
 
 export const BluetoothIcon: FC<SVGAttributes<SVGElement>> = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M17.71 7.71L12 2h-1v7.59L6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 11 14.41V22h1l5.71-5.71-4.3-4.29 4.3-4.29zM13 5.83l1.88 1.88L13 9.59V5.83zm1.88 10.46L13 18.17v-3.76l1.88 1.88z"/>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M17.71 7.71L12 2h-1v7.59L6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 11 14.41V22h1l5.71-5.71-4.3-4.29 4.3-4.29zM13 5.83l1.88 1.88L13 9.59V5.83zm1.88 10.46L13 18.17v-3.76l1.88 1.88z" />
   </svg>
 );
 
 export const HeadsetIcon: FC<SVGAttributes<SVGElement>> = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M12 1c-4.97 0-9 4.03-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7c0-4.97-4.03-9-9-9z"/>
+    <path d="M12 1c-4.97 0-9 4.03-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7c0-4.97-4.03-9-9-9z" />
   </svg>
 );
 
 export const GamepadIcon: FC<SVGAttributes<SVGElement>> = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-10 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5S14.67 12 15.5 12s1.5.67 1.5 1.5S16.33 15 15.5 15zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 10 19.5 10s1.5.67 1.5 1.5S20.33 12 19.5 12z"/>
+    <path d="M21 6H3c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-10 7H8v3H6v-3H3v-2h3V8h2v3h3v2zm4.5 2c-.83 0-1.5-.67-1.5-1.5S14.67 12 15.5 12s1.5.67 1.5 1.5S16.33 15 15.5 15zm4-3c-.83 0-1.5-.67-1.5-1.5S18.67 10 19.5 10s1.5.67 1.5 1.5S20.33 12 19.5 12z" />
   </svg>
 );
 
 export const KeyboardIcon: FC<SVGAttributes<SVGElement>> = props => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"/>
+    <path d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z" />
   </svg>
 );
--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,27 +1,18 @@
 import { DialogButton, Spinner as SDSpinner } from '@decky/ui';
 import { ImSpinner11 } from 'react-icons/im';
 
-export function Spinner({
-  loading = false,
-  refresh,
-}: {
-  loading: boolean;
-  refresh: () => void;
-}) {
+export function Spinner({ loading = false, refresh }: { loading: boolean; refresh: () => void }) {
   const style: React.CSSProperties = {
     minWidth: 0,
     padding: '6px 6px 2px 6px',
     borderRadius: '50%',
   };
 
-  return (
-    loading ?
-      <SDSpinner style={{ padding: '4px 4px 2px 0' }}/> :
-      <DialogButton
-        style={style}
-        onOKButton={refresh}
-        onClick={refresh}>
-        <ImSpinner11/>
-      </DialogButton>
+  return loading ? (
+    <SDSpinner style={{ padding: '4px 4px 2px 0' }} />
+  ) : (
+    <DialogButton style={style} onOKButton={refresh} onClick={refresh}>
+      <ImSpinner11 />
+    </DialogButton>
   );
 }
